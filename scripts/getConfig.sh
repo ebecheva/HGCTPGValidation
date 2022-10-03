416f7eb5b3cdd3bbcb0984ffa0956fe9accb992f
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2022-10-03 17:15:36 +0200

    source cmssw config env file for getting the default python version

--- scripts/getConfig.sh
+++ scripts/getConfig.sh
@@ -8,8 +8,9 @@
 echo "subset_config " $1
 echo "label " $2
 
-module purge
-module load python/3.7.0
+#module purge
+#module load python/3.7.0
+source /cvmfs/cms.cern.ch/cmsset_default.sh
 python --version
 python ./read_configuration_Jenkins.py --subsetconfig $1 --label $2
 
