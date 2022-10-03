c8bd586563d33b00680a1433b9642bdee6073ba8
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2022-10-03 17:38:46 +0200

    Set the path to the config file

--- scripts/read_configuration_Jenkins.py
+++ scripts/read_configuration_Jenkins.py
@@ -26,7 +26,7 @@ def read_subset(config):
 def read_config(config):
     
     filename = config + '.yaml'
-    with open('../../' + filename) as f:
+    with open('../../../HGCTPGValidation/config/' + filename) as f:
         config = yaml.full_load(f)
         
         #for item, config in config.items():
