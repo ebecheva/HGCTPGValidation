48b98d3d3fed63353ceac34927002e4090bfb55e
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2022-10-04 14:49:45 +0200

    add debur messages

--- scripts/read_configuration_Jenkins.py
+++ scripts/read_configuration_Jenkins.py
@@ -50,7 +50,8 @@ def main(subsetconfig, release):
     elif release=="test":
         print("Read config for test release")
         config_data=read_config(test)
-
+    pprint.pprint('=>nbEvents= ',nbEvents)
+    pprint.pprint('=>conditions= ',conditions)
     os.putenv("nbEvents",str(config_data['parameters']['nbOfEvents']))
     os.putenv("conditions",str(config_data['parameters']['conditions']))
     os.putenv("beamspot",str(config_data['parameters']['beamspot']))
