59f73d0419a62198c9aa62a4985b79b84483a3c3
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2022-10-04 09:39:08 +0200

    Change the python version

--- scripts/getConfig.sh
+++ scripts/getConfig.sh
@@ -8,9 +8,11 @@
 echo "subset_config " $1
 echo "label " $2
 
-#module purge
-#module load python/3.7.0
-source /cvmfs/cms.cern.ch/cmsset_default.sh
+module use /opt/exp_soft/vo.llr.in2p3.fr/modulefiles_el7/
+module avail
+module purge
+module load python/3.9.9
+
 python --version
 python ../../../HGCTPGValidation/scripts/read_configuration_Jenkins.py --subsetconfig $1 --label $2
 
