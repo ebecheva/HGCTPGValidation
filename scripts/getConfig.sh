e5d7b5393ac3e6ee0543ac96c6c9a11535183e52
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2022-10-04 11:43:18 +0200

    Add some debug messages

--- scripts/getConfig.sh
+++ scripts/getConfig.sh
@@ -15,4 +15,8 @@ module load python/3.9.9
 
 python --version
 python ../../../HGCTPGValidation/scripts/read_configuration_Jenkins.py --subsetconfig $1 --label $2
+echo '!!!!!!!!! 1 nbEvents= ' $nbEvents
+echo '!!!!!!!!! 1 conditions= ' $conditions
+echo '!!!!!!!!! 1 geometry= ' $geometry
+
 
