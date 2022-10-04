c7adffd26d44031a2aa35c2a01fa7c22dd2669c5
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2022-10-04 16:16:06 +0200

    remove print messages

--- scripts/getConfig.sh
+++ scripts/getConfig.sh
@@ -15,9 +15,6 @@ module load python/3.9.9
 
 python --version
 python ../../../HGCTPGValidation/scripts/read_configuration_Jenkins.py --subsetconfig $1 --label $2
-exit
-echo '!!!!!!!!! 1 nbEvents= ' $nbEvents
-echo '!!!!!!!!! 1 conditions= ' $conditions
-echo '!!!!!!!!! 1 geometry= ' $geometry
+
 
 
