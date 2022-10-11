c61472c1d89e3d964fe4a4c03fa2ce84d5b7ae5f
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2022-10-11 20:04:29 +0200

    Testing without run_cmsDriver(..) method

--- scripts/produceData_from_configuration.py
+++ scripts/produceData_from_configuration.py
@@ -98,7 +98,45 @@ def main(subsetconfig, release):
         config_data=read_config(test)
 
     pprint.pprint('Call cmsDriver')
-    command = run_cmsDriver(config_data, release)
+    #command = run_cmsDriver(config_data, release)
+    #---------- for test
+    nbEvents=configdata['parameters']['nbOfEvents']
+    conditions=configdata['parameters']['conditions']
+    beamspot=configdata['parameters']['beamspot']
+    geometry=configdata['parameters']['geometry']
+    era=configdata['parameters']['era']
+    inputCommands=configdata['parameters']['inputCommands']
+    procModifiers=configdata['parameters']['procModifiers']
+    filein=configdata['parameters']['filein']
+    customise=configdata['parameters']['customise_commands']
+
+    if procModifiers == 'empty':
+        command = 'echo $PWD; source /cvmfs/cms.cern.ch/cmsset_default.sh; eval `scramv1 runtime -sh`; ' + \
+        'cmsDriver.py hgcal_tpg_validation -n ' + str(nbEvents) + \
+        ' --mc --eventcontent FEVTDEBUG --datatier GEN-SIM-DIGI-RAW ' + \
+        '--conditions ' + conditions + ' ' + \
+        '--beamspot ' + beamspot + ' ' + \
+        '--step USER:Validation/HGCalValidation/hgcalRunEmulatorValidationTPG_cff.hgcalTPGRunEmulatorValidation ' + \
+        '--geometry ' + geometry + ' ' + '--era ' + era + ' ' + \
+        '--inputCommands ' + inputCommands + ' ' + \
+        '--filein ' + filein + ' ' + \
+        '--no_output ' + \
+        '--customise_commands ' + customise + ' ' + '"process.MessageLogger.files.out_"' + release + '" = dict(); process.Timing = cms.Service(\'Timing\', summaryOnly = cms.untracked.bool(False), useJobReport = cms.untracked.bool(True)); process.SimpleMemoryCheck = cms.Service(\'SimpleMemoryCheck\', ignoreTotal = cms.untracked.int32(1)); process.schedule = cms.Schedule(process.user_step)"'
+    else:
+        command = 'echo $PWD; source /cvmfs/cms.cern.ch/cmsset_default.sh; eval `scramv1 runtime -sh`;' + \
+        'cmsDriver.py hgcal_tpg_validation -n ' + str(nbEvents) + \
+        ' --mc --eventcontent FEVTDEBUG --datatier GEN-SIM-DIGI-RAW ' + \
+        '--conditions ' + conditions + ' ' + \
+        '--beamspot ' + beamspot + ' ' + \
+        '--step USER:Validation/HGCalValidation/hgcalRunEmulatorValidationTPG_cff.hgcalTPGRunEmulatorValidation ' + \
+        '--geometry ' + geometry + ' ' + '--era ' + era + ' ' + '--inputCommands ' + inputCommands + ' ' + \
+        '--procModifiers ' + procModifiers + ' ' + \
+        '--inputCommands ' + inputCommands + ' ' + \
+        '--filein ' + filein + ' ' + \
+        '--no_output ' + \
+        '--customise_commands ' + customise + ' ' + '"process.MessageLogger.files.out_"' + release + '"= dict(); process.Timing = cms.Service(\'Timing\', summaryOnly = cms.untracked.bool(False), useJobReport = cms.untracked.bool(True)); process.SimpleMemoryCheck = cms.Service(\'SimpleMemoryCheck\', ignoreTotal = cms.untracked.int32(1)); process.schedule = cms.Schedule(process.user_step)"'
+    pprint.pprint(command)
+    #---------------------
     sourceCmd = ['bash', '-c', command]
     sourceProc = subprocess.Popen(sourceCmd, stdout=logfile, stderr=logfile)
     (out, err) = sourceProc.communicate() # wait for subprocess to finish
