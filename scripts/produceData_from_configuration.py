ea5f74fadb9274ca863b352631470772b36cd6d1
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-10-12 11:49:30 +0200

    Check the type of a variable

--- scripts/produceData_from_configuration.py
+++ scripts/produceData_from_configuration.py
@@ -108,6 +108,7 @@ def main(subsetconfig, release):
     pprint.pprint('Call cmsDriver')
     #command = run_cmsDriver(config_data, release)
     #---------- for test
+    print(type(config_data))
     nbEvents=config_data['parameters']['nbOfEvents']
     conditions=config_data['parameters']['conditions']
     beamspot=config_data['parameters']['beamspot']
