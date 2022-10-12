cda22ffb6b934d5aad3ea651fd6c9ea92c332b5d
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-10-12 16:59:56 +0200

    typo,

--- scripts/produceData_from_configuration.py
+++ scripts/produceData_from_configuration.py
@@ -100,7 +100,7 @@ def main(subsetconfig, release):
         print("Read config for test release")
         config_data = read_config(test)
         print(type(config_data))
-        print(type(configdata['parameters']['conditions']))
+        print(type(config_data['parameters']['conditions']))
         print(type(config_data['parameters']['nbOfEvents']))
         pprint.pprint(config_data['parameters']['nbOfEvents'])
         pprint.pprint(nbEvents)
