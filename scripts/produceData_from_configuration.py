25edc9166d2ea8886b17daa608b9d08cfd81a0d9
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-10-12 10:24:25 +0200

    Add str for printing an int

--- scripts/produceData_from_configuration.py
+++ scripts/produceData_from_configuration.py
@@ -31,7 +31,7 @@ def read_config(config):
         config = yaml.full_load(f)
         
         nbEvents=config['parameters']['nbOfEvents']
-        pprint.pprint('nbEvents ==> ', nbEvents)
+        pprint.pprint('nbEvents ==> ', str(nbEvents))
         os.system('echo python --version')
         
         for item, config in config.items():
@@ -102,7 +102,7 @@ def main(subsetconfig, release):
         config_data=read_config(test)
         os.system('echo python --version')
         nbEvents=config_data['parameters']['nbOfEvents']
-        pprint.pprint('nbEvents => ', nbEvents)
+        pprint.pprint('nbEvents => ', str(nbEvents))
 
     pprint.pprint('Call cmsDriver')
     #command = run_cmsDriver(config_data, release)
