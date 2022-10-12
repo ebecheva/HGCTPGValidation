b7e233ce666305c03ac380fe95c8336f0e90a179
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-10-12 14:10:24 +0200

    Add debug message

--- scripts/produceData_from_configuration.py
+++ scripts/produceData_from_configuration.py
@@ -99,7 +99,7 @@ def main(subsetconfig, release):
     elif release=="test":
         print("Read config for test release")
         config_data=read_config(test)
-        os.system('python --version')
+        print(type(config_data))
         print("Print test config from main")
         for item, config_data in config_data.items():
             print(item, ":", config_data)
