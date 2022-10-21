2cc5d94eaed835485d2398b6f415ab5e44fe3d60
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-10-21 14:36:19 +0200

    Correct the path to the config files

--- scripts/produceData_from_configuration_orig.py
+++ scripts/produceData_from_configuration_orig.py
@@ -15,7 +15,7 @@ def read_subset(config):
     filename = config + '.yaml'
     print('filename = ', filename)
        
-    with open('../../' + filename) as f:
+    with open('../../../HGCTPGValidation/config/' + filename) as f:
         subset = yaml.full_load(f)
      
         for item, config in subset.items():
@@ -27,7 +27,7 @@ def read_subset(config):
 def read_config(config):
     
     filename = config + '.yaml'
-    with open('../../' + filename) as f:
+    with open('../../../HGCTPGValidation/config/' + filename) as f:
         config = yaml.full_load(f)
         
         #for item, config in config.items():
