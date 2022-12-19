dec44f378d408349f8e1191a137d386a8467bfb4
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-12-19 15:42:31 +0100

    Correct the path

--- scripts/produceData_multiconfiguration.py
+++ scripts/produceData_multiconfiguration.py
@@ -59,7 +59,7 @@ def read_subset(config):
     print('filename = ', filename)
     
     # for production case: with open('../../../HGCTPGValidation/config/' + filename) as f:
-    with open('./config/' + filename) as f:
+    with open('../../config/' + filename) as f:
         try:
             subset = yaml.safe_load(f)
             print("Read subset configuration file.")
@@ -74,7 +74,7 @@ def read_config(configuration):
     os.system('python --version')
     filename = configuration + '.yaml'
     #for production case: with open('../../../HGCTPGValidation/config/' + filename) as f:
-    with open('./config/' + filename) as f:
+    with open('../../config/' + filename) as f:
         try:
             config = yaml.safe_load(f)
             print("Read simulation configuration file.")
