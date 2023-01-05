a15afa2747bea13a65f456170329d9b5ceba12c7
ebecheva <emilia.becheva@llr.in2p3.fr>
2023-01-05 22:17:53 +0100

    Set a correct path

--- scripts/displayHistos.py
+++ scripts/displayHistos.py
@@ -62,7 +62,7 @@ def read_subset(config):
     filename = config + '.yaml'
     print('filename = ', filename)
     
-    with open('./HGCTPGValidation/config/' + filename) as f:
+    with open('../HGCTPGValidation/config/' + filename) as f:
         try:
             subset = yaml.safe_load(f)
         except yaml.YAMLError as e:
