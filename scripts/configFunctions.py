210185f80f4e701d7d9d82619295f3e048a54dee
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2023-03-06 19:40:31 +0100

    Correct typo

--- scripts/configFunctions.py
+++ scripts/configFunctions.py
@@ -39,7 +39,7 @@ def check_schema_config(config, filename):
             "inputCommands": str,
             "procModifiers": str,
             "filein": str,
-            "customise": str
+            "customise": str,
             "customise_commands": str
         }
     })
