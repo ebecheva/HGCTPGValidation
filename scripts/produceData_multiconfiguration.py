ecc3198e545b660f8ae8b292fb452b841c915867
ebecheva <emilia.becheva@llr.in2p3.fr>
2023-04-24 17:14:06 +0200

    Source cmssw setup

--- scripts/produceData_multiconfiguration.py
+++ scripts/produceData_multiconfiguration.py
@@ -39,7 +39,7 @@ def run_cmsDriver(configdata, label, release_path):
     customise = f'{"" if customiseUser=="empty" else f"--customise {customiseUser}"}'
     print("2 Current dir=", os.getcwd())
     
-    command = ("echo $PWD; " + "cd " + release_path + "; " +
+    command = ("echo $PWD; " + "cd " + release_path + "; source /cvmfs/cms.cern.ch/cmsset_default.sh; " +
     "eval `scramv1 runtime -sh`; echo $PATH; "
     "cmsDriver.py hgcal_tpg_validation_" + configName + "_" + label + "-n " + str(nbEvents) +
     " --mc --eventcontent FEVTDEBUG --datatier GEN-SIM-DIGI-RAW " +
