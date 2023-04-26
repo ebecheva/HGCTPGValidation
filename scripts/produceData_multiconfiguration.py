75ba16f3a3dcf0d7d20a3b79b7c34bf1f5ec0763
ebecheva <emilia.becheva@llr.in2p3.fr>
2023-04-26 15:04:50 +0200

    Remove shell=True

--- scripts/produceData_multiconfiguration.py
+++ scripts/produceData_multiconfiguration.py
@@ -88,7 +88,7 @@ def main(subsetconfig, label, topdir, release):
                 sourceCmd = ['/bin/bash', '-c', command]
                 #sourceProc = subprocess.Popen(sourceCmd, stdout=logfile, stderr=logfile)
                 #(out, err) = sourceProc.communicate() # wait for subprocess to finish
-                sourceProc = subprocess.run(sourceCmd, stdout=logfile, stderr=logfile, shell=True, check=True)
+                sourceProc = subprocess.run(sourceCmd, stdout=logfile, stderr=logfile, check=True)
                 #os.system(command)
             else:
               print("Do not run this configuration: ", key, ": ", value)
