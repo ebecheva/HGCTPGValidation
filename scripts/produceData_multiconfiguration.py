a1205483f7aa0133736d6e0e856f15441174a08c
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2024-09-06 15:12:09 +0200

    Remove redirecting stdout to stderr after get_rss_memory.sh

--- scripts/produceData_multiconfiguration.py
+++ scripts/produceData_multiconfiguration.py
@@ -47,7 +47,7 @@ def run_cmsDriver(configdata, release):
     --filein {filein} \
     --no_output \
     {customise} \
-    --customise_commands {customiseCommand} & ../../../HGCTPGValidation/scripts/get_rss_memory.sh $! 10 1000000 1>&2"
+    --customise_commands {customiseCommand} & ../../../HGCTPGValidation/scripts/get_rss_memory.sh $! 10 1000000"
     
     pprint.pprint(command)
     return command
